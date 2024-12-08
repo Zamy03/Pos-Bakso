00587a7f78d23bacbcf2c87b6a80596455c1d310
thxinsomnia <kompenizeko@gmail.com>
2024-12-08 15:05:10 +0700

    Add login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './main.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './page/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
